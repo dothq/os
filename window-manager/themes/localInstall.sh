78dab0cf21c0f2ba9982cf3bb1c2654e01b98c5c
trickypr <trickypr@users.noreply.github.com>
2021-01-16 17:25:13 +1100

    :bug: Correct dev script

--- window-manager/themes/localInstall.sh
+++ window-manager/themes/localInstall.sh
@@ -2,12 +2,12 @@
 # This is intended for development as the themes should be stored
 # in the system directory (/etc/icewm/themes/) in production.
 
-rm -rf "~/.icewm/themes/dotOS Light/"
-rm -rf "~/.icewm/themes/dotOS Dark/"
-mkdir -p "~/.icewm/themes/dotOS Light/"
-mkdir -p "~/.icewm/themes/dotOS Dark/"
-cp -a ./light/. "~/.icewm/themes/dotOS Light/"
-cp -a ./dark/. "~/.icewm/themes/dotOS Dark/"
+rm -rf ~/.icewm/themes/dotOS\ Light/
+rm -rf ~/.icewm/themes/dotOS\ Dark/
+mkdir -p ~/.icewm/themes/dotOS\ Light/
+mkdir -p ~/.icewm/themes/dotOS\ Dark/
+cp -a ./light/. ~/.icewm/themes/dotOS\ Light/
+cp -a ./dark/. ~/.icewm/themes/dotOS\ Dark/
 
 icewmbg -p &
 echo reload && icewm -r
