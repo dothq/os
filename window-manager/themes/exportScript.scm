9a86233084eac87dc28c24801254aa54dbaef3b3
trickypr <trickypr@users.noreply.github.com>
2021-01-17 16:00:33 +1100

    :sparkles: Script to automate generating both light and dark files from svgs

--- window-manager/themes/exportScript.scm
+++ window-manager/themes/exportScript.scm
@@ -1,15 +1,12 @@
 (
-  (let* ((filelist (cadr (file-glob "tmp/*.svg" 1))))
-
-  (while (not (null? filelist))
-  (
-    let* (
-      (filename (car filelist))
-      (image (car (file-svg-load RUN-NONINTERACTIVE filename filename 0 0 0 1)))
-      (drawable (car (gimp-image-get-active-layer image)))
-    ) 
-    
-    (file-xpm-save RUN-NONINTERACTIVE image drawable filename filename 255)
-  ))
+  let* (
+    (image (car (file-svg-load RUN-NONINTERACTIVE filename filename 0 0 0 1)))
+    (drawable (car (gimp-image-get-active-layer image)))
+  ) 
+  
+  (file-xpm-save RUN-NONINTERACTIVE image drawable filename filename 255)
 )
+
+(let* ((image (car (file-svg-load RUN-NONINTERACTIVE "tmp/close.svg" "tmp/close.svg" 0 0 0 1))) (drawable (car (gimp-image-get-active-layer image)))) (file-xpm-save RUN-NONINTERACTIVE image drawable "tmp/close.xpm" "tmp/close.xpm" 255))
+
 
--- window-manager/themes/iconExport.sh
+++ window-manager/themes/iconExport.sh
@@ -1,7 +1,49 @@
+icons=(close depth hide maximize menuButton minimize restore rolldown rollup)
+
 cd icons
 
 rm -rf tmp
 mkdir -p tmp
 cp *.svg tmp/
 
-gimp --verbose --batch='((let* ((filelist (cadr (file-glob "tmp/*.svg" 1)))) (while (not (null? filelist)) (let* ((filename (car filelist)) (image (car (file-svg-load RUN-NONINTERACTIVE filename filename 0 0 0 1))) (drawable (car (gimp-image-get-active-layer image)))) (file-xpm-save RUN-NONINTERACTIVE image drawable filename filename 255))))' -b '(gimp-quit 0)'
+exportArgs=""
+
+for i in "${icons[@]}"; do
+  fileSvg="tmp/${i}.svg"
+  fileXpm="tmp/${i}.xpm"
+
+  fileExport="-b '(let* ((image (car (file-svg-load RUN-NONINTERACTIVE \"${fileSvg}\" \"${fileSvg}\" 0 0 0 1))) (drawable (car (gimp-image-get-active-layer image)))) (file-xpm-save RUN-NONINTERACTIVE image drawable \"${fileXpm}\" \"${fileXpm}\" 255))'"
+  exportArgs="${exportArgs} ${fileExport}"
+done
+
+gimpCommand="gimp ${exportArgs} -b '(gimp-quit 0)'"
+eval $gimpCommand
+
+for i in "${icons[@]}"; do
+  rm "../light/${i}A.xpm" "../light/${i}I.xpm"
+
+  mv "tmp/${i}.xpm" "../light/${i}A.xpm"
+  cp "../light/${i}A.xpm" "../light/${i}I.xpm"
+done
+
+exportArgs=""
+
+for i in "${icons[@]}"; do
+  fileSvg="tmp/${i}.svg"
+  fileXpm="tmp/${i}.xpm"
+
+  fileExport="-b '(let* ((image (car (file-svg-load RUN-NONINTERACTIVE \"${fileSvg}\" \"${fileSvg}\" 0 0 0 1))) (drawable (car (gimp-image-get-active-layer image)))) (gimp-drawable-invert drawable TRUE) (file-xpm-save RUN-NONINTERACTIVE image drawable \"${fileXpm}\" \"${fileXpm}\" 255))'"
+  exportArgs="${exportArgs} ${fileExport}"
+done
+
+gimpCommand="gimp ${exportArgs} -b '(gimp-quit 0)'"
+eval $gimpCommand
+
+for i in "${icons[@]}"; do
+  rm "../dark/${i}A.xpm" "../dark/${i}I.xpm"
+
+  mv "tmp/${i}.xpm" "../dark/${i}A.xpm"
+  cp "../dark/${i}A.xpm" "../dark/${i}I.xpm"
+done
+
+rm -rf tmp
