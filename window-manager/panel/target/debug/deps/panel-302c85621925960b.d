422b89dc21922812427001b6cea2aa33dbe274b4
trickypr <trickypr@users.noreply.github.com>
2021-01-18 14:23:11 +1100

    🔥 Remove rust target files

--- window-manager/panel/target/debug/deps/panel-302c85621925960b.d
+++
@@ -1,5 +0,0 @@
-/home/main/Documents/dothq/os/window-manager/panel/target/debug/deps/panel-302c85621925960b.rmeta: src/main.rs
-
-/home/main/Documents/dothq/os/window-manager/panel/target/debug/deps/panel-302c85621925960b.d: src/main.rs
-
-src/main.rs:
--- window-manager/panel/target/debug/deps/panel-4e89c0d1077b35f4.d
+++
@@ -1,5 +0,0 @@
-/home/main/Documents/dothq/os/window-manager/panel/target/debug/deps/panel-4e89c0d1077b35f4.rmeta: src/main.rs
-
-/home/main/Documents/dothq/os/window-manager/panel/target/debug/deps/panel-4e89c0d1077b35f4.d: src/main.rs
-
-src/main.rs:
